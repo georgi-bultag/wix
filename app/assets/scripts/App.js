91807ad4a584d8f5f221e06a1e10547e652e2db1
joro <georgi.bultag@gmail.com>
2019-08-20 18:26:35 +0300

    almost done

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,9 @@
+var john = {
+	name: "John Doe",
+	favoriteColor: "blue",
+	greet: function() {
+		console.log("Oh hello, my name is " + john.name + "and my fav color is " + john.favoriteColor + ".")
+	}
+}
+
+john.greet()
