809ed0ef37f2d224b825ae85dd8e4b762042cf6e
joro <georgi.bultag@gmail.com>
2019-08-30 14:20:30 +0300

    updated on

--- app/assets/scripts/scripts.js
+++ app/assets/scripts/scripts.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
-    $(".menu-item-has-children").click(function(){
-    	$(this).toggleClass("menu-item-has-children--expanded");
+    $(".span").click(function(){
+    	$(this).toggleClass("&:before, &:after");
   });
 });
 
